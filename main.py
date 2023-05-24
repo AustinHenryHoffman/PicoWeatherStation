a0d0469b2ad2e966a14a9ad75197f8f8cd5610fa
DrTautology <austinhenryhoffman@gmail.com>
2023-05-24 15:01:12 -0500

    Inital Commit

---
+++ main.py
@@ -0,0 +1,65 @@
+import random
+import utime
+import st7789
+import tft_config
+import vga2_bold_16x32 as font
+import network   # handles connecting to WiFi
+import urequests    # handles making and servicing network requests
+from machine import Pin, I2C
+import ahtx0
+
+tft = tft_config.config(3)
+# Connect to network
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+# Fill in your network name (ssid) and password here:
+ssid = 'Tauttechsystems'
+password = 'Ellajane4485!'
+wlan.connect(ssid, password)
+
+
+def center(text):
+    length = 1 if isinstance(text, int) else len(text)
+    tft.text(
+        font,
+        text,
+        tft.width() // 2 - length // 2 * font.WIDTH,
+        tft.height() // 2 - font.HEIGHT // 2,
+        st7789.GREEN,
+        st7789.BLUE)
+
+
+def get_current_forecast():
+    r = urequests.get("http://api.weatherapi.com/v1/forecast.json?key=1523873bc3d04c4f823185542232405&q=63031&days=2&aqi=no&alerts=no")
+    weather_data = r.json()
+    #temp = data['current']['temp_f']
+    #looks = data['current']['text']
+    return weather_data
+
+
+def main():
+
+    tft.init()
+    tft.fill(st7789.BLUE)
+    weather_data = get_current_forecast()
+
+    while True:
+        r = urequests.get("http://192.168.1.4:5000")  # Server that returns the current GMT+0 time.
+        date = r.json()["date"]
+        time = r.json()["time"]
+        tft.text(font, date, 80, 0, st7789.GREEN, st7789.BLUE)
+        tft.text(font, time, 90, 30, st7789.GREEN, st7789.BLUE)
+        tft.text(font, str(weather_data['current']['condition']['text']), 0, 100, st7789.GREEN, st7789.BLUE)
+        #tft.text(font, (f"{weather_data['current']['temp_f']} F"), 0, 130, st7789.GREEN, st7789.BLUE)
+        tft.text(font, (f"Max Temp:{weather_data['forecast']['forecastday'][0]['day']['maxtemp_f']} F"), 0, 130, st7789.GREEN, st7789.BLUE)
+        tft.text(font, (f"Min Temp:{weather_data['forecast']['forecastday'][0]['day']['mintemp_f']} F"), 0, 160, st7789.GREEN, st7789.BLUE)
+
+    #while True:
+        #tft.fill(st7789.RED)
+        #center("Hello")
+        #utime.sleep(2)
+    #tft.fill(st7789.BLACK)
+
+
+main()
